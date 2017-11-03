cde6ccd5277429b06a4b028447278acd04bbd106
ZEUS1977 <piergiorgio.buragina@gmail.com>
2017-11-03 20:56:03 +0100

    react bootstrap

--- scripts/init.sh
+++ scripts/init.sh
@@ -9,6 +9,11 @@ sudo npm install webpack -g;
 sudo npm install --save babel-core babel-loader@6.2.10 babel-preset-react@6.23.0 babel-preset-es2015@6.18.0 babel-preset-stage-1@6.16.0 redux express;
 
 sudo npm install --save-dev webpack;
+sudo npm install react --save;
+sudo npm install react-dom --save;
+sudo npm install react-router@3.0.5 --save;
+sudo npm install axios --save;
+sudo npm install es6-promise --save;
 
 cp ./scripts/webpack.config.js ./;
 
@@ -19,3 +24,13 @@ cp ./scripts/index.html ./public;
 cp ./scripts/server.js ./;
 
 cp ./scripts/.gitignore ./;
+
+cp -f ./scripts/webpack.config.js ./
+
+mkdir src;
+
+cp ./scripts/app.js ./src;
+
+node server.js&;
+
+webpack;
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  watch: true,
+
+  devServer: {
+     inline: true,
+     port: 8080
+  },
+
+  module: {
+     loaders: [
+        {
+           test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      }
+    ]
+  }
+}
